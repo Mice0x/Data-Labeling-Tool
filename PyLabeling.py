e5825877fe8f040f82c8f1aecd46dad033bd1ac3
mice0x <tobiasorthofer267@gmail.com>
2020-10-04 16:53:18 +0200

    Category Creation Menu

--- PyLabeling.py
+++ PyLabeling.py
@@ -1,24 +1,56 @@
 from tkinter import *
-  
+from tkinter.colorchooser import *
+
 class Window(Frame):
     def __init__(self, master=None):
-        Frame.__init__(self, master)        
+        Frame.__init__(self, master)
+        self.color = '#d9d9d9'
         self.master = master
         self.option_add('*Font', '19')
-        # widget can take all window
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=4)
 
-
-        ButtonArr = [0 for y in range(5)]
-        for y in range(5):
-            ButtonArr[y] = Button(self)
-            ButtonArr[y].grid(column = 0, row = y)
-        AddButton = Button(self, text="+", command=self.AddCategory)
+        #declaring the Top Level Window
+        self.newWindow = Toplevel()
+        self.newWindow.destroy()
         
-        # place button at (0,0)
-        AddButton.grid(column=0, row=len(ButtonArr))
+        self.ButtonArr = []
+
+        self.AddButton = Button(self, text="+", command=self.CreateCategory)
+        self.AddButton.grid(column=0, row=len(self.ButtonArr))
+
+    def CreateCategory(self):
+        
+        
+        if Toplevel.winfo_exists(self.newWindow):
+            self.newWindow.destroy()
+        self.newWindow = Toplevel(self)
+        self.newWindow.wm_title("Create a new Category")
+        self.newWindow.geometry("400x330")
+        self.newWindow.option_add('*Font', '19')
+        L1 = Label(self.newWindow, text="Label")
+        L1.pack(side=LEFT)
+        E1 = Entry(self.newWindow, bd=5, font=("Arial", 14))
+        E1.pack(side=RIGHT)
+        self.ColerChooser = Button(self.newWindow,text="Select Color" ,command=self.getColor).pack(side=TOP)
+        
+
+        self.ButtonArr.append(Button(self, text=E1.get()))
+        
+        self.ok = Button(self.newWindow, text="Confirm",command = self.AddCategory(), bg='green').pack(side=BOTTOM)
+        
+        
+    def getColor(self):
+        self.color = askcolor()
+        self.newWindow.configure(background=self.color[1])
+        print(self.color)
+        
     def AddCategory(self):
-        pass
+        
+        
+        self.AddButton.grid(column=0, row=len(self.ButtonArr) + 1)
+
+        self.ButtonArr[len(self.ButtonArr) - 1].grid(column=0,row=len(self.ButtonArr))
+        
 root = Tk()
 app = Window(root)
 root.wm_title("Button Test")
