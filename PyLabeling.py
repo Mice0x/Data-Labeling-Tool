00fe86f292d4f432bcbc7bb7e0afccf89b3b6fb7
mice0x <tobiasorthofer267@gmail.com>
2020-10-04 10:18:01 +0200

    Button Array

--- PyLabeling.py
+++ PyLabeling.py
@@ -1,7 +1,26 @@
 from tkinter import *
   
-root = Tk() 
-a = Label(root, text ="Hello World") 
-a.pack() 
-  
-root.mainloop() 
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)        
+        self.master = master
+        self.option_add('*Font', '19')
+        # widget can take all window
+        self.pack(fill=BOTH, expand=1)
+
+
+        ButtonArr = [0 for y in range(5)]
+        for y in range(5):
+            ButtonArr[y] = Button(self)
+            ButtonArr[y].grid(column = 0, row = y)
+        AddButton = Button(self, text="+", command=self.AddCategory)
+        
+        # place button at (0,0)
+        AddButton.grid(column=0, row=len(ButtonArr))
+    def AddCategory(self):
+        pass
+root = Tk()
+app = Window(root)
+root.wm_title("Button Test")
+root.geometry("320x320")
+root.mainloop()
